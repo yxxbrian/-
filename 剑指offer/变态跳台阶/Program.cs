65090648213a86b025e3faeed05984a50afc1aff
yxxbrian <1505566462@qq.com>
2017-08-22 17:19:48 +0800

    add all

---
+++ 剑指offer/变态跳台阶/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 变态跳台阶
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        class Solution
+        {
+            public int jumpFloorII(int number)
+            {
+                int[] dp = new int[number + 1];
+                dp[1] = 1;
+                // write code here
+                for (int i = 2; i <= number; i++)
+                {
+                    for (int j = i; j >= 1 ; j--)
+                    {
+                        dp[i] += dp[i - j];
+                    }
+                }
+                return dp[number];
+            }
+        }
+
+    }
+}
