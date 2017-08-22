9548a3c8e7fe1291c0a200dab3d9a49532b0adf7
yxxbrian <1505566462@qq.com>
2017-08-22 16:50:50 +0800

    all all

---
+++ 剑指offer/斐波那契数列/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 斐波那契数列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = int.Parse(Console.ReadLine());
+            int result = Fibonacci(index);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        //运行时间：25ms 占用内存：2632k
+        /*
+        public int Fibonacci(int n)
+        {
+            // write code here
+            int index = n;
+            int[] array = new int[40];
+            int tmp = 1;
+            array[tmp] = 1;
+            while (tmp++ < index)
+            {
+                array[tmp] = array[tmp - 1] + array[tmp - 2];
+            }
+            return array[index];
+        }
+        */
+        public int Fibonacci(int n) 
+        {
+            int num1 = 1;
+            int num2 = 0;
+            int tmp = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                tmp = num1 + num2;
+                num2 = num1;
+                num1 = tmp;
+            }
+            return num1;
+        }
+
+
+
+    }
+}
---
+++ 剑指offer/跳台阶/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 跳台阶
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int jumpFloor(int number)
+        {
+            // write code here
+            if (number == 1 || number == 0)
+                return 1;
+            int dp_1 = 1;
+            int dp_2 = 1;
+            int tmp = 0;
+            for (int i = 2; i <= number; i++)
+            {
+                tmp = dp_1 + dp_2;
+                dp_2 = dp_1;
+                dp_1 = tmp;
+            }
+            return dp_1;
+        }
+
+
+    }
+}
