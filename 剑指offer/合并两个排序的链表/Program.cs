7a3f4e8a1f091dcd9bba5d639e4afd74d32f5fe0
yxxbrian <1505566462@qq.com>
2017-08-30 10:13:12 +0800

    add all

--- 剑指offer/合并两个排序的链表/Program.cs
+++ 剑指offer/合并两个排序的链表/Program.cs
@@ -25,6 +25,33 @@ namespace 合并两个排序的链表
                 val = x;
             }
         }
+        class Solution1
+        {
+            public ListNode Merge(ListNode pHead1, ListNode pHead2)
+            {
+                // write code here
+                if (pHead1 == null)
+                    return pHead2;
+                else if (pHead2 == null)
+                    return pHead1;
+                else
+                {
+                    if (pHead1.val < pHead2.val)
+                    {
+                        pHead1.next = Merge(pHead1.next, pHead2);
+                        return pHead1;
+                    }
+                    else
+                    {
+                        pHead2.next = Merge(pHead1, pHead2.next);
+                        return pHead2;
+                    }
+                }
+            }
+        }
+
+
+        //24 ms	2536K
         class Solution
         {
             public ListNode Merge(ListNode pHead1, ListNode pHead2)
@@ -42,10 +69,39 @@ namespace 合并两个排序的链表
                     ListNode cur2 = pHead2;
                     ListNode curDst = dst;
                     if (cur1.val < cur2.val)
+                    {
                         curDst = cur1;
+                        cur1 = cur1.next;
+                    }
                     else
+                    {
                         curDst = cur2;
+                        cur2 = cur2.next;
+                    }
                     dst = curDst;
+
+
+                    while (cur1 != null && cur2 != null)
+                    {
+                        if (cur1.val < cur2.val)
+                        {
+                            curDst.next = cur1;
+                            curDst = curDst.next;
+                            cur1 = cur1.next;
+                        }
+                        else
+                        {
+                            curDst.next = cur2;
+                            curDst = curDst.next;
+                            cur2 = cur2.next;
+                        }
+                    }
+                    if (cur1 != null)
+                        curDst.next = cur1;
+                    else
+                        curDst.next = cur2;
+                    return dst;
+
 
                 }
             }
